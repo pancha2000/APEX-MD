8e482c935b70f5829e4009ae7b2653a87b426bea
pancha2000 <119848240+pancha2000@users.noreply.github.com>
2025-06-01 18:15:02 +0530

    update config.js

--- config.js
+++ config.js
@@ -5,5 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID "7NsUERBT#2EKn99NqkNzmZOFzQqm_UIqZ3GQ4igpQ56eduKCmoBg",
+ALIVE_IMG: process.env.ALIVE_IMG || "img url",
+ALIVE_MSG: process.env.ALIVE_MSG || "IM ALIVE",
 };
