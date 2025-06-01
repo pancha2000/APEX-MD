8143ac3717bf0aa8b3570a5c2b7d313508e631e0
pancha2000 <119848240+pancha2000@users.noreply.github.com>
2025-06-01 18:50:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID = "7NsUERBT#2EKn99NqkNzmZOFzQqm_UIqZ3GQ4igpQ56eduKCmoBg",
-ALIVE_IMG: process.env.ALIVE_IMG || "img url",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/pancha2000/Pancha-one-bot/blob/main/Img/Bobcat-643-Operation-Maintenance-Manual-1.png",
 ALIVE_MSG: process.env.ALIVE_MSG || "IM ALIVE",
 };
