b8bd80230bd3c7d5f31b4c3d49ec48e5ecd55996
pancha2000 <119848240+pancha2000@users.noreply.github.com>
2025-06-05 18:15:47 +0530

    update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ if (fs.existsSync('config.env')) {
 // If you need it later, you can re-add it.
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "eJt1STZT#mGNSYtvJeTlqOgT-uervalGfVqhqsxitbACRYzPoS4c",
+    SESSION_ID: process.env.SESSION_ID || "PRlQWQCC#kbL4tob-unW5vicqtShvwBJMX_sq6g00FHwkgT2LCdg",
     MONGODB: process.env.MONGODB || "mongodb+srv://pancha:2006.Shehan@cluster0.hyugfei.mongodb.net/",
     GEMINI_API_KEY: process.env.GEMINI_API_KEY || "AIzaSyCItRq9qKhyDo5ZjO_ZBtRC1Z-Y3UD9Ma0",
 };
