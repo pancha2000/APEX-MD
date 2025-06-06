ad985bc3d7b868099e3fb0141ae6aa602141a8a2
pancha2000 <119848240+pancha2000@users.noreply.github.com>
2025-06-06 10:10:16 +0530

    update config.js

--- config.js
+++ config.js
@@ -7,7 +7,18 @@ if (fs.existsSync('config.env')) {
 // If you need it later, you can re-add it.
 
 module.exports = {
+  //===================Bot info===============================
     SESSION_ID: process.env.SESSION_ID || "PRlQWQCC#kbL4tob-unW5vicqtShvwBJMX_sq6g00FHwkgT2LCdg",
     MONGODB: process.env.MONGODB || "mongodb+srv://pancha:2006.Shehan@cluster0.hyugfei.mongodb.net/",
     GEMINI_API_KEY: process.env.GEMINI_API_KEY || "AIzaSyCItRq9qKhyDo5ZjO_ZBtRC1Z-Y3UD9Ma0",
+    
+// ==================Your info=================================
+
+  OWNER_NAME: "Shehan Vimukthi",
+  PUBLIC_NAME: "Real Pancha",
+  NICKNAME: "Pancha",
+  AGE: "19",
+  OWNER_CONTACT: "wa.me/+94701391585",
+  ALIVE_IMG: "https://your-image-link.com/image.jpg"
+
 };
