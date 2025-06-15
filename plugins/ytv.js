636058cf9c29c9a2bf1a11d43a6f252d17d47d50
pancha2000 <cdilrukshi52@gmail.com>
2025-06-15 11:22:37 +0530

    update plugins/ytv.js

--- plugins/ytv.js
+++ plugins/ytv.js
@@ -30,7 +30,9 @@ cmd(
 👻 Channel : ${data.author.name}
 👻 Link : ${data.url}
 
-MADE BY SHEHAN VIMUKYHI`;
+Powered by APEX-MD
+
+.ytmp command එක වැඩ නැතිනම් අනෙක් download command උත්සහ කරන්න';
 
       await conn.sendMessage(
         from,
